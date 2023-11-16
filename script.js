48efeafe0f9540ceb3b6e0c85941fafc1ee3c959
Caetano-Ventura-Coutinho <currytano@gmail.com>
2023-11-15 22:33:21 -0300

    Slideshow funcionando

---
+++ script.js
@@ -0,0 +1,17 @@
+let imagens = ['img/slideshow1.png', 'img/slideshow2.png', 'img/slideshow3.png'];
+let Index = 0;
+let time = 5000;
+
+function slideShow() {
+
+    document.getElementById('slideshow').src = imagens[Index];
+    Index++;
+
+     if (Index == imagens.length) {
+    Index = 0;
+  }
+
+  setTimeout('slideShow()', time);
+}
+
+slideShow();
